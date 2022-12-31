fde5b6f89dcea562ad1304ea4959dfab95d563a9
Victor <thelegendofvictor@gmail.com>
2022-12-31 15:20:11 -0600

    Created 8 files

---
+++ style.js
@@ -0,0 +1,11 @@
+//1. Create a function
+//2. Function will take two arguments
+//3.function is gonna return an array
+function countByX(param1, param2) {
+  const array = [];
+  for (let i = 1; i <= param2; i++) {
+    console.log(i);
+  }
+}
+
+countByX(1);
